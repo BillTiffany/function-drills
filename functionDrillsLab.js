1d32c6f7b56f17f7886fbf1af8b7234a88294b6d
Bill Tiffany <daganlegalservice@gmail.com>
2021-09-22 16:51:13 -0600

    not yet finished

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -219,7 +219,7 @@ let sampleString = "Hi, my name is Kylo."
 */
 
 //CODE HERE
-function upperString (str){
+function upperString(str){
   console.log(str.toUpperCase)
 }
 upperString(sampleString)
@@ -282,13 +282,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["duck", "rubberDuck"]
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ['duck', "rubberduck", "sailorDuck"]
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ['duck', "realDuck" ]
